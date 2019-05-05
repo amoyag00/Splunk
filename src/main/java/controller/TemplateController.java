6429880c1513e46e0b1f4b7f807297aa56c9cc6f
amoyag00 <amoyag00@estudiantes.unileon.es>
2019-05-05 19:20:45 +0200

    access control partially implemented

--- src/main/java/controller/TemplateController.java
+++ src/main/java/controller/TemplateController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import java.io.IOException;
 import java.io.Serializable;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import model.User;
 
 /**
  *
@@ -12,4 +17,19 @@ import javax.inject.Named;
 @ViewScoped
 public class TemplateController implements Serializable{
     
+    public void checkPermissions(){
+       User us =(User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
+       HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+       //TODO check if this url has rol user.gerRol().getRolId()
+      // String path = request.getRequestURI().substring(request.getContextPath().length());
+      // System.out.println("URL: "+path);
+        
+       if(us==null){
+           try {
+               FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath()+"/public/error.xhtml?faces-redirect=true");
+           } catch (IOException ex) {
+               System.out.println("Error al redireccionar");
+           }
+       }
+    }
 }
